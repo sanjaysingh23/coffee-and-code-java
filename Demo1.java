b43d5c9d728785e3df7dc6913db30bfb78a590df
Sanjay Singh <109591671+sanjaysingh23@users.noreply.github.com>
2023-10-23 17:01:12 +0530

    Add files via upload

--- Demo1.java
+++ Demo1.java
@@ -1,15 +1,20 @@
-class Base{
-    static {
-        System.out.println("In base disp");
+class MyClass{
+    public String toString(){
+        return "["+"Myclass"+"]";
     }
 }
-class Sub extends Base{
-    static {
-        System.out.println("In sub disp");
+class Everything{
+    void disp(Object ...arg){
+        for(int i=0; i<arg.length; i++){
+            System.out.println(arg[i]);
+        }
     }
 }
 public class Demo1 {
-    public static void main(String arr[]){
-        Sub s = new Sub();
+    public static void main(String args[]){
+        String s = "xyz";
+        Everything e = new Everything();
+        MyClass m = new MyClass();
+        e.disp(23,"abc",s, new MyClass(),m);
     }
 }
--- Demo2.java
+++ Demo2.java
@@ -1,18 +1,54 @@
-class Vehicle{
-    void start(){
-        System.out.println("Vehicle disp");
+ class Sample {
+    char ch;
+    Sample(char ch){
+        this.ch = ch;
     }
-}
-class FourWheeler extends Vehicle{
-    void start(){
-        System.out.println("FourWheeler disp");
+    public int hashCode(){
+        return Character.hashCode(ch);
     }
+    public String toString(){
+        return String.valueOf(ch);
+    }
+    public boolean equals(Object obj)
+    {
+        if(this == obj)
+        {
+            System.out.println("inside true");
+            return true;
+        }
+        if(obj == null || getClass() != obj.getClass())
+        {
+            System.out.println("inside false");
+            return false;
+        }
+
+        Sample ref = (Sample)obj;
+        return ch == ref.ch; 
+        
+    }
+   
 }
-public class Demo2 {
-    public static void main(String args[]){
-        Vehicle v1= new Vehicle();
-        v1.start();
-        Vehicle v = new FourWheeler();
-        v.start();
-    } 
+public class Demo2
+{
+     public static void main(String arr[]){
+        Demo2 d2 = new Demo2();
+        Sample s1 = new Sample('A');
+        Sample s2 = new Sample('A');
+        // System.out.println(s1.ch);
+        // System.out.println(s2.ch);
+        System.out.println(s1);
+        System.out.println(s2);
+
+        s1.equals(d2);
+
+        if(s1.equals(s1)){
+            System.out.println("True");
+            System.out.println(s1.hashCode()+" "+s2.hashCode());
+        }
+        else{
+            System.out.println("False");
+            System.out.println(s1.hashCode()+" "+s2.hashCode());
+            System.out.println("Both passed charactors are different");
+        }
+    }
 }
